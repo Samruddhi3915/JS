8add3c3b9dbb30c6c2fdf3c49511c75a1110d61f
Samruddhi3915 <samruddhi3915@gmail.com>
2023-02-20 23:37:24 +0530

    JS Repo

---
+++ 06Advanceish/01Scope.js
@@ -0,0 +1,6 @@
+// console.log(name);
+// var name = "samruddhi";
+if (true) {
+  let lastname = "Ahire";
+}
+console.log(lastname);
---
+++ 06Advanceish/02Strings.js
@@ -0,0 +1,9 @@
+var name = "Samruddhi";
+
+var fullname = `${name} Ahire?`;
+console.log(fullname);
+
+console.log(fullname.charAt(0));
+console.log(fullname.endsWith("?"));
+console.log(fullname.includes("Sa"));
+console.log(fullname.toUpperCase());
