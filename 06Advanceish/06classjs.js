afec2fc247703a1f2b2369fc37f527fa307030fe
Samruddhi3915 <samruddhi3915@gmail.com>
2023-02-23 23:33:30 +0530

    Updating JS repo

--- 06Advanceish/06classjs.js
+++ 06Advanceish/06classjs.js
@@ -13,12 +13,31 @@ class User {
   getCourseList() {
     return this.#courseList;
   }
+  login() {
+    return "you are logged in";
+  }
+}
+class subAdmin extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+  getAdminInfo() {
+    return "I am subadmin";
+  }
+  login() {
+    return "login for admin only";
+  }
 }
 
 module.exports = User;
 
-const rock = new User("rock", "rock@gmail.com");
-console.log(rock.getInfo());
-rock.enrollCourse("C++");
-console.log(rock.getCourseList());
-console.log(rock.courseList);
+// const rock = new User("rock", "rock@gmail.com");
+// console.log(rock.getInfo());
+// rock.enrollCourse("C++");
+// console.log(rock.getCourseList());
+// console.log(rock.courseList);
+
+const tom = new subAdmin("tom", "tom@");
+console.log(tom.getAdminInfo());
+console.log(tom.getInfo());
+console.log(tom.login());
